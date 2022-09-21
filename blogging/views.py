7ba69bc7e8a8a3a7133c4ca9839a433f62066dff
colinflynnak <colinflynnak@gmail.com>
2022-09-20 22:44:00 -0700

    Code style enforcement

--- blogging/views.py
+++ blogging/views.py
@@ -8,16 +8,20 @@ from django.shortcuts import get_object_or_404
 
 
 class BlogListView(ListView):
-    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
-    context_object_name = 'posts'
-    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by(
+        "-published_date"
+    )
+    context_object_name = "posts"
+    template_name = "blogging/list.html"
 
 
 class BlogDetailView(DetailView):
     # queryset = Post.objects.exclude(published_date__exact=None)
-    context_object_name = 'post'
-    template_name = 'blogging/detail.html'
+    context_object_name = "post"
+    template_name = "blogging/detail.html"
 
     def get_queryset(self):
-        self.post = get_object_or_404(Post, pk=self.kwargs['pk'])  # self.post is a model object, based on the subsequent query (pk = xxx)
+        self.post = get_object_or_404(
+            Post, pk=self.kwargs["pk"]
+        )  # self.post is a model object, based on the subsequent query (pk = xxx)
         return Post.objects.exclude(published_date__exact=None).filter(pk=self.post.pk)
--- mysite/heroku.py
+++ mysite/heroku.py
@@ -18,4 +18,3 @@ MIDDLEWARE = (
     "whitenoise.middleware.WhiteNoiseMiddleware",
     *MIDDLEWARE,
 )
-
